325a6a371d8f4e6f284d2d18de36d204402555bb
bigblackbug <bigblackbugg@gmail.com>
2017-09-19 21:18:20 +0300

    fixed test startup waiting on a database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-until psql $DATABASE_URL -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - continuing"
+if [[ ${DATABASE_URL} ]]; then
+    echo "${DATABASE_URL} is set"
+    until psql $DATABASE_URL -c '\l'; do
+      >&2 echo "Postgres is unavailable - sleeping"
+      sleep 1
+    done
+    >&2 echo "Postgres is up - continuing"
+fi
 
 if [ "X$DJANGO_RUN_MIGRATIONS" = 'Xyes' ]; then
     echo "running migrations"
