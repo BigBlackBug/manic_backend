d6114ea062194b2a595229bc8dc3f5c5a96d3ab5
bigblackbug <bigblackbugg@gmail.com>
2017-10-31 01:05:53 +0300

    added logging for categories app

--- src/apps/categories/receivers.py
+++ src/apps/categories/receivers.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+import logging
 import os
 
 from django.db import models
 from django.dispatch import receiver
 
 from .models import ServiceCategory
+
+logger = logging.getLogger(__name__)
 
 
 # These two auto-delete files from filesystem when they are unneeded:
@@ -16,6 +19,7 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
     """
     if instance.image:
         if os.path.isfile(instance.image.path):
+            logger.info(f'Removing image of category {instance.name}')
             os.remove(instance.image.path)
 
 
@@ -33,6 +37,8 @@ def auto_delete_file_on_change(sender, instance, **kwargs):
         old_file = ServiceCategory.objects.get(pk=instance.pk).image
         # if there was no file associated with the category
         if not old_file:
+            logger.info(f'No image was associated with '
+                        f'category {instance.name}')
             return False
     except ServiceCategory.DoesNotExist:
         return False
@@ -40,4 +46,5 @@ def auto_delete_file_on_change(sender, instance, **kwargs):
     new_file = instance.image
     if not old_file == new_file:
         if os.path.isfile(old_file.path):
+            logger.info(f'Removing image of category {instance.name}')
             os.remove(old_file.path)
