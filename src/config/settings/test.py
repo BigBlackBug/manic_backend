7a2a693e353c0673afd978c7e1e5dc85d644de16
bigblackbug <bigblackbugg@gmail.com>
2017-11-03 07:56:14 +0300

    fixed test config

--- src/config/settings/test.py
+++ src/config/settings/test.py
@@ -1,13 +1,14 @@
 from .base import *
 
 DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '4hands2go_local',
-        'USER': '4hands2go_local',
-        'PASSWORD': '4hands2go_local',
-        'HOST': 'localhost',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': get_env_variable('DATABASE_HOST'),
         'PORT': '5432',
     }
 }
