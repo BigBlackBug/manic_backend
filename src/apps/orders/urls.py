214b60c02f2168999eda5b41287484d2ff97a2d0
bigblackbug <bigblackbugg@gmail.com>
2017-11-03 08:07:41 +0300

    fixed an url

--- src/apps/orders/urls.py
+++ src/apps/orders/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
         name=FinishS3DView.view_name),
     url(r'^(?P<pk>[0-9]+)/complete$', CompleteOrderView.as_view(),
         name=CompleteOrderView.view_name),
-    url(r'^cloudpayments_transaction/(?P<transaction_id>[0-9]+)',
+    url(r'^cloudpayments_transaction/(?P<transaction_id>[0-9]+)$',
         CloudPaymentsTransactionView.as_view(),
         name=CloudPaymentsTransactionView.view_name),
 ]
