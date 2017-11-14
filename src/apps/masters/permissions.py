bcab75c9d12954b603e4011aa6d83cb4aae8fd32
bigblackbug <bigblackbugg@gmail.com>
2017-11-14 14:45:37 +0530

    added a IsMasterVerified permission

--- src/apps/masters/permissions.py
+++ src/apps/masters/permissions.py
@@ -1,4 +1,5 @@
 from src.apps.core.permissions import IsMaster
+from .models import MasterStatus
 
 
 class IsMasterIDCorrect(IsMaster):
@@ -6,7 +7,21 @@ class IsMasterIDCorrect(IsMaster):
     Allows access only to users which are masters and whose id
     matches with url id
     """
+    message = "Trying to access someone else's endpoint"
 
     def has_permission(self, request, view):
         return super().has_permission(request, view) and \
                int(view.kwargs['pk']) == request.user.master.id
+
+
+class IsMasterVerified(IsMaster):
+    """
+    Allows access only to users which are masters and whose id
+    matches with url id
+    """
+    message = "Master is unverified, please contact " \
+              "the 4hands2go Administrator"
+
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and \
+               request.user.master.status == MasterStatus.VERIFIED
