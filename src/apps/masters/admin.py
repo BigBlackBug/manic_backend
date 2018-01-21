afd2c741282777c268958c6c47de9cf7e5004ec9
bigblackbug <bigblackbugg@gmail.com>
2018-01-21 15:07:41 +0300

    added Feedback to admin page

--- src/apps/masters/admin.py
+++ src/apps/masters/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Schedule, TimeSlot, Master
+from .models import Schedule, TimeSlot, Master, Feedback
 
 admin.site.register(Master)
 admin.site.register(Schedule)
 admin.site.register(TimeSlot)
+admin.site.register(Feedback)
