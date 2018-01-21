e8f8752e89d0934737c70879baef405ab929b855
bigblackbug <bigblackbugg@gmail.com>
2018-01-21 15:41:27 +0300

    added Balance to admin

--- src/apps/masters/admin.py
+++ src/apps/masters/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Schedule, TimeSlot, Master, Feedback, PortfolioImage
+from .models import Schedule, TimeSlot, Master, Feedback, PortfolioImage, \
+    Balance
 
 admin.site.register(Master)
+admin.site.register(Balance)
 admin.site.register(Schedule)
 admin.site.register(TimeSlot)
 admin.site.register(Feedback)
