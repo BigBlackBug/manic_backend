3a67e308fc07a04e497df9d5103f0b91c746fd06
bigblackbug <bigblackbugg@gmail.com>
2018-01-21 15:38:25 +0300

    added PortfolioItem to admin

--- src/apps/masters/admin.py
+++ src/apps/masters/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Schedule, TimeSlot, Master, Feedback
+from .models import Schedule, TimeSlot, Master, Feedback, PortfolioImage
 
 admin.site.register(Master)
 admin.site.register(Schedule)
 admin.site.register(TimeSlot)
 admin.site.register(Feedback)
+admin.site.register(PortfolioImage)
